0138649ca729b6b36240cbac5820a5f01460a7bb
Renan Pires <renan.sp.121@hotmail.com>
2022-08-26 22:54:00 -0300

    26/08/2022

--- Sistemas Distribuidos e Mobile/Aula03(18-08-2022)/Exercicio01/src/databaseConnection/Connector.java
+++ Sistemas Distribuidos e Mobile/Aula03(18-08-2022)/Exercicio01/src/databaseConnection/Connector.java
@@ -4,7 +4,7 @@ public class Connector {
 	public static Connection getConnection() {
 		Connection connection = null;
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bancodb","root","13579Rock");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bancodb","root","");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
