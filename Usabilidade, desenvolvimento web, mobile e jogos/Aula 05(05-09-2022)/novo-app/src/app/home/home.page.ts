60e39bc109715d725451aefa1106685dcb3b4a1f
Renan Pires <renan.sp.121@hotmail.com>
2022-09-05 22:05:02 -0300

    05/09/2022

---
+++ Usabilidade, desenvolvimento web, mobile e jogos/Aula 05(05-09-2022)/novo-app/src/app/home/home.page.ts
@@ -0,0 +1,47 @@
+/*Importação do decorator que será utilizado para criar componentes.
+Este decorator faz parte do núcleo do Angular.*/
+import { Component } from '@angular/core';
+
+interface IPessoa {
+  nome: string;
+  sobrenome: string;
+}
+
+
+//Decorator do Angular responsável por criar componentes.
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+//Criação da classe que serve de base para o nosso componente "Home".
+export class HomePage {
+
+  //Propriedade utilizado no primeiro exemplo de interpolação.
+  public texto = 'Boa noite pessoal, segue então nosso ex. de interpolação!'
+
+    //Propriedade que implementa a interface "IPessoa". O conteúdo é exibido na "Toolbar".
+    public pessoa: IPessoa = {
+      nome: 'Yuri',
+      sobrenome: 'Yowtuber'
+    };
+  
+    public cor = 'green'
+
+    public currentValue = ''
+
+    public switch = true;
+
+    public valorNumerico = 0;
+
+    aumentar(): void {
+      if (this.valorNumerico < 10) this.valorNumerico++;
+    }
+    diminuir(): void {
+      if (this.valorNumerico > 0) this.valorNumerico--;
+    }
+    resetar(): void {
+      this.valorNumerico = 0;
+    }
+}
