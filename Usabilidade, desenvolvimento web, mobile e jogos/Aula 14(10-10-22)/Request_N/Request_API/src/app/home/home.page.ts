6a539b74b4701ef911e1e38660168aa1bda242bf
Renan Pires <renan.sp.121@hotmail.com>
2022-10-11 14:34:40 -0300

    11/10/2022

--- Usabilidade, desenvolvimento web, mobile e jogos/Aula 14(10-10-22)/Request_N/Request_API/src/app/home/home.page.ts
+++ Usabilidade, desenvolvimento web, mobile e jogos/Aula 14(10-10-22)/Request_N/Request_API/src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface IProduct {
+  id? : number
+  name : string
+  price : number
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +13,60 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public url = 'http://localhost:3333/products'
+  public products: IProduct[] = []
+  public id : number
+  public name : string
+  public price : number
+
+  constructor() {
+    this.buscar()
+  }
+
+  async buscar(): Promise<void> {
+    const resposta = await fetch(this.url)
+    this.products = await resposta.json()
+  }
+
+  async buscarPorId(id: number): Promise<void> {
+    const resposta = await fetch(`${this.url}/${id}`);
+    const product: IProduct = await resposta.json();
+
+    this.name = product.name;
+    this.price = product.price;
+  }
+
+  async salvar(): Promise<void> {
+    const novo = {
+      name: this.name,
+      price: this.price
+    }
+    console.log(Object.keys(novo))
+    const body = Object.keys(novo)
+    .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(novo[k])}`)
+    .join(`&`)
+
+    await fetch(this.url, { method:'POST', body: new URLSearchParams(body) })
+    this.buscar()
+  }
+
+  async atualizar(id: number): Promise<void> {
+    const produtoAtualizado = {
+      name: this.name,
+      price: this.price
+    }
+    console.log(Object.keys(produtoAtualizado))
+    const body = Object.keys(produtoAtualizado)
+    .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(produtoAtualizado[k])}`)
+    .join(`&`)
+
+    await fetch(this.url, { method:'PUT', body: new URLSearchParams(body) })
+    this.buscar()
+  }
+  
+  async remover(id: number): Promise<void> {
+    await fetch(`${this.url}/${id}`, {method: 'DELETE'})
+    this.buscar()
+  }
 
 }
